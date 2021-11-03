b3c0c6b98e8e2a7d162176d46684636c1ac7fd12
swingcloud <sam@swingcloud.app>
2021-11-03 10:11:29 +0800

    initialize

---
+++ lib/exqueue/application.ex
@@ -0,0 +1,20 @@
+defmodule Exqueue.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Exqueue.Worker.start_link(arg)
+      # {Exqueue.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Exqueue.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
